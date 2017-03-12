2ae60aa1d0d213eb9d600d97fadbbdf9ebdb0664
s tech <email@sarcas.tech>
2017-03-12 15:43:32 -0400

    add tests for requestHandler.js

--- test/requestHandlerSpec.js
+++ test/requestHandlerSpec.js
@@ -2,14 +2,62 @@
 
 const test = require('ava')
 const handler = require('../lib/requestHandler')
-const fakeStore = {
+const sinon = require('sinon')
+let fakeStore = {
   find: function () {
-    return {foo: 'bar'}
+    return {
+      'foo': 'bar'
+    }
   }
 }
+let fakeReq = {
+  url: '/my/fake/url'
+}
+let fakeRes
+let req
 
-const req = handler(fakeStore)
+test.beforeEach((t) => {
+  req = handler(fakeStore)
+  sinon.stub(console, 'log', function (val) {
+    t.context.console = val
+  })
 
-test('requestHandler should return a function', function (t) {
+  fakeRes = {
+    setHeader: sinon.spy(),
+    end: sinon.spy()
+  }
+})
+
+test.afterEach((t) => {
+  req = null
+  console.log.restore()
+  fakeRes = {}
+})
+
+test('requestHandler should return a middleware function', function (t) {
   t.is(typeof req, 'function')
 })
+
+test('Middleware function should set correct headers', function (t) {
+  req(fakeReq, fakeRes)
+  t.is(fakeRes.setHeader.firstCall.calledWith('Content-Type', 'application/json'), true)
+  t.is(fakeRes.setHeader.secondCall.calledWith('Access-Control-Allow-Origin', '*'), true)
+  t.is(fakeRes.setHeader.thirdCall.calledWith('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'), true)
+})
+
+test('When passed valid path, should return json data', function (t) {
+  req(fakeReq, fakeRes)
+  t.is(fakeRes.end.args[0][0].includes('foo'), true)
+})
+
+test('When passed invalid path, should return 404 msg', function (t) {
+  req = handler({
+    find: function () {
+      return false
+    }
+  })
+  req(fakeReq, fakeRes)
+
+  t.is(fakeRes.end.calledWith('404 - not found'), true)
+})
+
